6c5c1341b9cb43f3cac2ba1050d02ede9efdfba2
Mohammed Ajmal Siddiqui <ajmalsiddiqui21@gmail.com>
2018-03-17 15:16:33 +0530

    Integrated gensim textrank summarizer

--- app.py
+++ app.py
@@ -2,13 +2,28 @@
 This contains the Flask server-side code for the application
 '''
 
-from flask import Flask
+# Summarization services
+from services.summarizers import gensimTextRank
+
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return "Hello"
+
+@app.route('/summarize', methods = ['POST'])
+def summarize():
+	data = request.get_json()
+	text = data['text']
+	title = data['title']
+	summary = gensimTextRank(text, data)
+	response = {
+		'summary': summary
+	}
+	return json.dumps(response)
+
 
 print(__name__)
 if __name__ == "__main__":
